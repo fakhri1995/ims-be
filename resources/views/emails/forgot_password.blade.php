0793890c8be23e245e96e5a5b99124c8553ca502
Averlyn <mfmakarim@gmail.com>
2022-01-14 11:24:22 +0700

    Edit Link Reset Password Email

--- resources/views/emails/forgot_password.blade.php
+++ resources/views/emails/forgot_password.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 
-Silahkan ubah password anda pada <a href="{{ url('https://migsys.herokuapp.com/forgetPassword?token='.$data['token']) }}">link ini</a>  <br>
+Silahkan ubah password anda pada <a href="{{ url('https://migsys.herokuapp.com/resetPassword?token='.$data['token']) }}">link ini</a>  <br>
 
 Salam,<br>
 {{ config('app.name') }}
