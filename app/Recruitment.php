b0f79ed6ac000a9e60f3c13fb9318299af70ee72
Patar Martua Doli Siahaan <109059171+patarshn-mig@users.noreply.github.com>
2022-10-10 14:11:37 +0700

    Update Recruitment

--- app/Recruitment.php
+++ app/Recruitment.php
@@ -9,4 +9,25 @@ class Recruitment extends Model
 {
     use SoftDeletes;
     public $timestamps = false;
+
+    public function role()
+    {
+        return $this->belongsTo(RecruitmentRole::class, 'recruitment_role_id');
+    }
+
+    public function jalur_daftar()
+    {
+        return $this->belongsTo(RecruitmentJalurDaftar::class, 'recruitment_jalur_daftar_id');
+    }
+
+    public function stage()
+    {
+        return $this->belongsTo(RecruitmentStage::class, 'recruitment_stage_id');
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(RecruitmentStatus::class, 'recruitment_status_id');
+    }
 }
+
