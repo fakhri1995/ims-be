b976ebe5d45b67146d3a3746d1188efcfb2988c9
patarshn-mig <patar@mitrasolusi.group>
2022-11-22 10:14:27 +0700

    Update RecruitmentService.php

--- app/Services/RecruitmentService.php
+++ app/Services/RecruitmentService.php
@@ -1883,7 +1883,7 @@ class RecruitmentService{
         
             $sendMail = Mail::to($email)->send(new RecruitmentMail($data));
         try{
-            return ["success" => true, "message" => $sendMail, "status" => 200];
+            return ["success" => true, "message" => "Email berhasil dikirim", "status" => 200];
         }catch(Exception $err){
             return ["success" => false, "message" => $err, "status" => 400];
         }
