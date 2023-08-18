f104ab5b8d6ed659c2cdb9e5a7cc279148d7eba8
patarshn-mig <109059171+patarshn-mig@users.noreply.github.com>
2023-08-18 13:33:36 +0700

    feat & fix(Contract Service): add new endpoint
    - add new endpoint for delete invoice
    - change relation name, change params

--- app/ContractInvoice.php
+++ app/ContractInvoice.php
@@ -3,17 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ContractInvoice extends Model
 {
     protected $casts = ['invoice_attribute' => 'object', 'service_attribute' => 'object'];
     public $timestamps = false;
+    use SoftDeletes;
 
     public function contract_template(){
        return $this->hasOne(Contract::class,'id','contract_template_id');
     }
 
-    public function invoice_services(){
+    public function service_attribute_values(){
         return $this->hasMany(ContractInvoiceProduct::class,'contract_invoice_id','id');
-     }
+    }  
 }
