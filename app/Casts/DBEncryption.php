bb0b99324b66899b54a50a05e4344924aaa32781
Sian <anaufal@mitrasolusi.group>
2023-10-18 10:18:12 +0700

    update(Encryption)
    - make possble to null value

--- app/Casts/DBEncryption.php
+++ app/Casts/DBEncryption.php
@@ -17,7 +17,7 @@ class DBEncryption implements CastsAttributes
      */
     public function get($model, string $key, $value, array $attributes)
     {
-        return openssl_decrypt(base64_decode($value), 'aes-128-ecb', config('encrypt.key'), OPENSSL_RAW_DATA);
+        return $value ? openssl_decrypt(base64_decode($value), 'aes-128-ecb', config('encrypt.key'), OPENSSL_RAW_DATA) : $value;
     }
 
     /**
@@ -31,6 +31,6 @@ class DBEncryption implements CastsAttributes
      */
     public function set($model, string $key, $value, array $attributes)
     {
-        return base64_encode(openssl_encrypt($value, 'aes-128-ecb', config('encrypt.key'), OPENSSL_RAW_DATA));
+        return $value ? base64_encode(openssl_encrypt($value, 'aes-128-ecb', config('encrypt.key'), OPENSSL_RAW_DATA)) : $value;
     }
 }
