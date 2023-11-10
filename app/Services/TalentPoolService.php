b828ce65079b826dfca23998e3f77a5c354e6d11
Sian <anaufal@mitrasolusi.group>
2023-11-10 09:42:21 +0700

    update(TalentPool)
    - improve logic api getTalentPools like api getTalentPoolSharePublics

--- app/Services/TalentPoolService.php
+++ app/Services/TalentPoolService.php
@@ -53,18 +53,15 @@ class TalentPoolService
             $talentPools = TalentPool::query()
                 ->with(["resume" => function ($q1) {
                     $q1
-                        ->with(["lastEducation" => function ($q2) {
-                            $q2->select('resume_id', 'university');
-                        }])
-                        ->with(["lastAssessment" => function ($q2) {
-                            $q2->select('id', 'name');
-                        }])
-                        ->with(["skills"])
+                        ->with("lastEducation")
+                        ->with("lastAssessment")
+                        ->with("skills")
+                        ->with("summaries")
                         ->with(["lastExperience" => function ($q2) {
-                            $q2->select(DB::raw('role, resume_id, CASE
-                            WHEN end_date IS NOT NULL THEN YEAR(end_date)
-                            ELSE YEAR(start_date)
-                            END AS year'));
+                            $q2->select(DB::raw('*, CASE
+                        WHEN end_date IS NOT NULL THEN YEAR(end_date)
+                        ELSE YEAR(start_date)
+                        END AS year'));
                         }])
                         ->with(['recruitment' => function ($q2) {
                             $q2->select('owner_id', 'created_at');
@@ -74,14 +71,12 @@ class TalentPoolService
                 $talentPools = $talentPools->whereHas("resume", function ($q1) use ($skill, $role, $year, $education) {
                     if ($education)
                         $q1->whereHas("lastEducation", function ($q2) use ($education) {
-                            $q2->select('resume_educations.resume_id', 'resume_educations.university');
                             if ($education) {
                                 $q2->whereIn('university', $education);
                             }
                         });
                     if ($role)
                         $q1->whereHas("lastAssessment", function ($q2) use ($role) {
-                            $q2->select('resume_assessments.id', 'resume_assessments.name');
                             if ($role) {
                                 $q2->whereIn('name', $role);
                             }
@@ -107,12 +102,10 @@ class TalentPoolService
                     if ($skill || $role || $year || $education || $keyword) {
                         $q1->where(function ($q2) use ($skill, $role, $education, $keyword, $year) {
                             $q2->orWhereHas("lastEducation", function ($q3) use ($keyword, $education) {
-                                $q3->select('resume_educations.resume_id', 'resume_educations.university');
                                 if ($education) $q3->whereIn('university', $education);
                                 if ($keyword) $q3->where('university', 'like', '%' . $keyword . '%');
                             })
                                 ->orWhereHas("lastAssessment", function ($q3) use ($keyword, $role) {
-                                    $q3->select('resume_assessments.id', 'resume_assessments.name');
                                     if ($role) $q3->whereIn('name', $role);
                                     if ($keyword) $q3->where('name', 'like', '%' . $keyword . '%');
                                 })
@@ -137,7 +130,7 @@ class TalentPoolService
             }
             $talentPools = $talentPools->whereHas('category', function (Builder $q1) {
                 $q1->where('company_id', auth()->user()->company_id);
-            })->where('talent_pool_category_id', $request->category_id);
+            })->where('talent_pool_category_id', $request->category_id)->latest('mark')->latest('id');
             if ($status) $talentPools = $talentPools->whereIn('status', $status);
             $rows = $request->rows ?? 10;
             $talentPools = $talentPools->paginate($rows);
