6182488d139184dd972bf40c92fb22119a61e3a4
Palito <palito@mitrasolusi.group>
2024-08-20 14:19:19 +0700

    AttendanceController: added getAttendanceFormCompany

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -35,6 +35,14 @@ class AttendanceController extends Controller
         return response()->json($response, $response['status']);
     }
 
+    public function getAttendanceFormCompany(Request $request)
+    {
+        $route_name = "ATTENDANCE_FORM_COMPANY_GET";
+
+        $response = $this->attendanceService->getAttendanceFormCompany($request, $route_name);
+        return response()->json($response, $response['status']);
+    }
+
     public function addAttendanceForm(Request $request)
     {
         $route_name = "ATTENDANCE_FORM_ADD";
